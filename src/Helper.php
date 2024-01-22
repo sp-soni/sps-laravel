90cbb2836541239de6690da8019a8466e5ac1a93
Pradnya Bhosale <pradnya.bhosale@panasonic.aero>
2024-01-23 03:47:10 +0530

    commit

--- src/Helper.php
+++ src/Helper.php
@@ -11,4 +11,22 @@ class Helper
             return 'selected';
         }
     }
+
+    public static function input(string $name, string $value, array $extra = [])
+    {
+        $aAttribute = [
+            'id' => $name,
+            'name' => $name,
+            'value' => $value,
+            'class' => 'form-control',
+            'type' => 'text'
+        ];
+        $aAttribute = array_merge($aAttribute, $extra);
+        $attribute = '';
+        foreach ($aAttribute as $property => $value)
+        {
+            $attribute .= $property . '="' . $value . '" ';
+        }
+        return '<input  ' . $attribute . '/>';
+    }
 }
